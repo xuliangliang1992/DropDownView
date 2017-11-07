b2ca82b3e13071a9062ef5fcc4c7e4b89d98e87e
xuliangliang <1299945061@qq.com>
2017-11-07 11:24:55 +0800

    first commit

---
+++ DropDownView/app/src/main/java/com/xll/dropdownview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.xll.dropdownview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
